11c8dde5a50b22434a094d1c8f4592ca2800be15
abenbyy <andrbenaya@gmail.com>
2021-07-16 23:33:51 +0700

    added CORS middleware

--- server.go
+++ server.go
@@ -8,8 +8,27 @@ import (
 )
 const defaultPort = "8080"
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Credentials", "true")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Header("Access-Control-Allow-Methods", "POST,HEAD,PATCH, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func main(){
 	r:= gin.Default()
+	r.Use(CORSMiddleware())
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
